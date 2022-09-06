5b3d086eb7c4c96fc5335d9f2faa60b9e3b0e006
Suresh Dukiya <rjsh1503@gmail.com>
2022-09-06 23:42:44 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import '../styles/App.css';
-const App = () => {
 
-  return (
-    <div id="main">
-      <div className="date-time"></div>
-    </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date()
+    };
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.setState({ time: new Date() });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  render() {
+    return (
+      <div id="main">
+        <div className="date-time">{this.state.time.toLocaleString()}</div>
+      </div>
+    )
+  }
 }
 
 
